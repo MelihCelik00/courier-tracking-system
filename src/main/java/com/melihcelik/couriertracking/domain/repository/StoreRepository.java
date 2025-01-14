bb8dbb3b25ff169ab3e691e5780efcda529c1b06
MelihCelik00 <melihsafa.c@gmail.com>
2025-01-14 15:16:13 +0300

    Revert "initial commit"
    This reverts commit dabcddc489f0d5a80b76ab8d9becd92b91d3556b.

--- src/main/java/com/melihcelik/couriertracking/domain/repository/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.melihcelik.couriertracking.domain.repository;
-
-import com.melihcelik.couriertracking.domain.model.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoreRepository extends JpaRepository<Store, Long> {
-} 
